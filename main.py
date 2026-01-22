17b051056967da906397e2fca2ea830a9e74e0e1
D.MITHESH <114052780+Mithesh6369894902@users.noreply.github.com>
2026-01-23 00:50:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from fastapi import FastAPI
-from api import inferno, text, vision, workflow, model
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI(
-    title="ÆTHERIUM AI Operating System",
-    version="1.0"
+from api import inferno, vision, text, workflow, model
+
+app = FastAPI(title="ÆTHERIUM AI Backend")
+
+# ---------------- CORS ---------------- #
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
+# ---------------- ROUTERS ---------------- #
 app.include_router(inferno.router)
-app.include_router(text.router)
 app.include_router(vision.router)
+app.include_router(text.router)
 app.include_router(workflow.router)
 app.include_router(model.router)
 
