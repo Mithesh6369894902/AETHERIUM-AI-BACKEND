0b7f62e5887ca57fb91863016ace73d5937142cd
D.MITHESH <114052780+Mithesh6369894902@users.noreply.github.com>
2026-01-22 20:14:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from fastapi import FastAPI
-from api import inferno, text, vision, model, workflow
+from api import inferno, text, vision, workflow, model
 
 app = FastAPI(
     title="ÆTHERIUM AI Operating System",
-    description="Unified AI Backend for Android & Desktop Apps",
     version="1.0"
 )
 
-app.include_router(inferno.router, prefix="/inferno", tags=["InfernoData"])
-app.include_router(text.router, prefix="/text", tags=["TextVortex"])
-app.include_router(vision.router, prefix="/vision", tags=["VisionBlaze"])
-app.include_router(model.router, prefix="/model", tags=["ModelCraftX"])
-app.include_router(workflow.router, prefix="/workflow", tags=["AlphaFlux"])
+app.include_router(inferno.router)
+app.include_router(text.router)
+app.include_router(vision.router)
+app.include_router(workflow.router)
+app.include_router(model.router)
 
 @app.get("/")
 def root():
-    return {"message": "ÆTHERIUM Backend is running"}
+    return {"status": "ÆTHERIUM backend running"}
