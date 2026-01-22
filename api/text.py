f2fb3e1ac8c705660beeba9881d13ca9eec10ae9
D.MITHESH <114052780+Mithesh6369894902@users.noreply.github.com>
2026-01-22 20:16:25 +0530

    Update text.py

--- api/text.py
+++ api/text.py
@@ -1,8 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from auth import verify_api_key
+from logger import log
 from modules.textvortex.core import sentiment_analysis
 
-router = APIRouter()
+router = APIRouter(prefix="/text", tags=["TextVortex"])
 
 @router.post("/sentiment")
-def sentiment(payload: dict):
-    return {"sentiment": sentiment_analysis(payload["text"])}
+def sentiment(payload: dict, dep=Depends(verify_api_key)):
+    log("TextVortex sentiment analysis executed")
+    return {
+        "sentiment": sentiment_analysis(payload["text"])
+    }
