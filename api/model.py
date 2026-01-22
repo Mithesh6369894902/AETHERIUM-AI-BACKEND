2aa733d05526362066f238bca32713759f731914
D.MITHESH <114052780+Mithesh6369894902@users.noreply.github.com>
2026-01-23 01:04:22 +0530

    Update model.py

--- api/model.py
+++ api/model.py
@@ -1,11 +1,10 @@
 from fastapi import APIRouter, Depends
 from auth import verify_api_key
-from logger import log
 from modules.modelcraftx.core import run_benchmark
 
 router = APIRouter(prefix="/modelcraft", tags=["ModelCraft-X"])
 
 @router.post("/benchmark")
 def benchmark(payload: dict, dep=Depends(verify_api_key)):
-    log("ModelCraft-X benchmark called")
     return run_benchmark(payload)
+
